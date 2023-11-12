c12fec85c7daebd1fc23dbbc1ca711a52d41ec4b
Infernaught <89timw@gmail.com>
2023-11-11 19:41:15 -0500

    update sdk

--- index/assets/js/sdk/apigClient.js
+++ index/assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://hk5v5cdowc.execute-api.us-east-1.amazonaws.com/dev';
+    var invokeUrl = 'https://hk5v5cdowc.execute-api.us-east-1.amazonaws.com/dev2';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
